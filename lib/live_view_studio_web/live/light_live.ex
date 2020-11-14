4e82c853813916249acc69d426c63ae845dddffd
SPrio <mahantasupriyo@gmail.com>
2020-11-14 15:58:31 +0530

    Checked liveview callbacks

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,15 +2,18 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
+    IO.puts "MOUNT #{inspect(self())}"
     {:ok, assign(socket, :brightness, 10)}
   end
 
   def handle_event("on", _, socket) do
+    IO.puts "ON #{inspect(self())}"
     socket = assign(socket, :brightness, 100)
     {:noreply, socket}
   end
 
   def handle_event("off", _, socket) do
+    IO.puts "OFF #{inspect(self())}"
     socket = assign(socket, :brightness, 0)
     {:noreply, socket}
   end
@@ -26,6 +29,7 @@ defmodule LiveViewStudioWeb.LightLive do
   end
 
   def render(assigns) do
+    IO.puts "RENDER #{inspect(self())}"
     ~L"""
     <h1>Front Porch Light</h1>
     <div class="meter">
